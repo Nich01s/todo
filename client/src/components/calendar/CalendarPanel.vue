cd40ecbc8e45eea8691431eaff02019b9cac2b26
nichao <17717033286@163.com>
2026-05-19 01:56:51 +0800

    Add DashboardView layout skeleton with stub child components (Task 16)

---
+++ client/src/components/calendar/CalendarPanel.vue
@@ -0,0 +1,1 @@
+<template><div class="calendar-panel">Calendar</div></template>
--- client/src/views/DashboardView.vue
+++ client/src/views/DashboardView.vue
@@ -1,1 +1,39 @@
-<template><div>Dashboard</div></template>
+<template>
+  <div class="dashboard">
+    <CalendarPanel class="dash-left" />
+    <div class="dash-right">
+      <WeatherWidget />
+      <StatsCard />
+      <QuickAddBar @added="refresh" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import CalendarPanel from '../components/calendar/CalendarPanel.vue'
+import WeatherWidget from '../components/weather/WeatherWidget.vue'
+import StatsCard from '../components/stats/StatsCard.vue'
+import QuickAddBar from '../components/todo/QuickAddBar.vue'
+
+const refreshKey = ref(0)
+function refresh() { refreshKey.value++ }
+</script>
+
+<style scoped>
+.dashboard {
+  display: flex;
+  gap: 14px;
+  padding: 16px;
+  height: calc(100vh - 56px);
+  overflow: hidden;
+}
+.dash-left { flex: 1; min-width: 0; }
+.dash-right {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  min-width: 0;
+}
+</style>
